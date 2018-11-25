1dc328905998e9e4a9c08107eb08a882e7d53510
ada-lenovo-1 <ada@ada-lenovo-1.ada.com.ar>
2018-11-25 02:25:00 -0300

    memotest terminado

--- tp-2/js/index.js
+++ tp-2/js/index.js
@@ -6,12 +6,13 @@ const lsRankingKey = "memotest-ranking";
 
 // Variables
 var nombre = "";
-var dificultad = 0;
+var dificultad = "";
 var intentosRestantes = 0;
 var paresEncontrados = 0;
 var firstCard = null;
 var secondCard = null;
-var ranking = [];
+// var ranking = [];
+var lsRankingString = "";
 var cards = [
   "imagenes/alce.jpg",
   "imagenes/alce.jpg",
@@ -34,8 +35,14 @@ var tarjetas = $('.tarjeta');
 var error = $('#error');
 var sectionInicio = $('.inicio');
 var tablero = $('#tablero');
-var lsRankingString = localStorage.getItem(lsRankingKey);
-
+var hola = $('#hola');
+var intentosTablero = $('#intentos');
+var intentosTitulo = $('#intentosTitulo');
+var nombreNivel = $('#nombreNivel');
+var rankingGanador = $("#rankingGanador");
+var rankingPerdedor = $("#rankingPerdedor"); 
+var botonVolver = $('.botonVolver');
+var tablaRanking = $('table');
 
 // Funcion de logueo e inicio
 botonesDificultad.on('click', function(e) {
@@ -49,28 +56,29 @@ botonesDificultad.on('click', function(e) {
     var dificultadId = e.target.id;
 
     setDificultad(dificultadId);
-    ocultarElementos(sectionInicio);
-    mostrarElementos(tablero);
+    hideElements(sectionInicio);
+    showElements(tablero);
 
-    $('#hola').html(nombre);
-    $('#intentos').html(intentosRestantes);
-    $('#intentosTit').html(intentosRestantes);
-    $('#nombreNivel').html(e.target.id);
+    hola.html(nombre);
+    intentosTablero.html(intentosRestantes);
+    intentosTitulo.html(intentosRestantes);
+    nombreNivel.html(e.target.id);
   }
 })
 
 // Funcion que muestra error de input vacio
 
 function showError() {
-  mostrarElementos(error);
+  showElements(error);
     setTimeout(function() {
-      ocultarElementos(error);
+      hideElements(error);
     }, 3000)
 }
 
 // Funcion que indica cuantos intentos hay por nivel
 
 function setDificultad(dificultadId) {
+  dificultad = dificultadId;
   if (dificultadId == "facil") {
     intentosRestantes = nivelFacil;
   } else if (dificultadId == "intermedio") {
@@ -129,51 +137,46 @@ function tocarCarta(e){
     }
     
     intentosRestantes--;
-    $('#intentos').html(intentosRestantes);
+    intentosTablero.html(intentosRestantes);
   }
 
   if (paresEncontrados == 6 && intentosRestantes >= 0) {
-    var tablero = $("#tablero");
-    var rankingGanador = $("#rankingGanador");
+    var ranking = calcularRanking({
+      nombre: nombre,
+      nivel: dificultad,
+      intentos: calcularPuntaje()
+    });
+    dibujarRanking(ranking);
+
     setTimeout(function(){
-      mostrarElemento(tablero);
-      mostrarElementos(rankingGanador);
+      addOpacity(tablero);
+      showElements(rankingGanador);
     },1300);
     
   }
   if (paresEncontrados < 6 && intentosRestantes == 0) {
-    var tablero = $("#tablero");
-    var rankingPerdedor = $("#rankingPerdedor"); 
-    mostrarElemento(tablero);
-    mostrarElementos(rankingPerdedor);
+    addOpacity(tablero);
+    showElements(rankingPerdedor);
   }
 }
-
-// // si el string NO es null y el string NO esta vacio
-// if (lsRankingString != null && lsRankingString != "") {
-//   // lo parseo y lo convierto en un array de objetos de ranking
-//   // y lo guardo en la variable "ranking"
-//   ranking = JSON.parse(lsRankingString);
-// }
-// console.log(ranking)
 
 
 // Funcion para volver al inicio y volver a jugar
 
-$('.botonVolver').on('click',function(e) {
+botonVolver.on('click',function(e) {
   // var rankingGanador = $("#rankingGanador");
   // var rankingPerdedor = $("#rankingPerdedor"); 
-  // ocultarElementos(rankingGanador, rankingPerdedor);
-  // ocultarElemento(sectionInicio);
-  // mostrarElementos(sectionInicio);
-  // ocultarElementos(tablero);
-  // ocultarElemento(tablero);
+  // hideElements(rankingGanador, rankingPerdedor);
+  // removeOpacity(sectionInicio);
+  // showElements(sectionInicio);
+  // hideElements(tablero);
+  // removeOpacity(tablero);
   // cards = shuffle(cards);
+  // newGame();
   location.reload();
 })
 
 // function newGame () {
-  
 //   // nombre = "";
 //   // dificultad = 0;
 //   // intentosRestantes = 0;
@@ -184,10 +187,51 @@ $('.botonVolver').on('click',function(e) {
 //   // tarjetas.removeClass('color');
 // }
 
+// Funcion para calcular el puntaje de la partida
+
+function calcularPuntaje() {
+  if (dificultad == "facil") {
+    return nivelFacil - intentosRestantes;
+  }
+  if (dificultad == "intermedio") {
+    return nivelIntermedio - intentosRestantes;
+  }
+  return nivelExperto - intentosRestantes;
+}
+
+// Funcion para calcular y actualizar el ranking desde local storage
+
+function calcularRanking(nuevoPuntaje) {
+  var lsRanking = localStorage.getItem(lsRankingKey);
+  var ranking = [];
+  if (lsRanking != null) {
+    ranking = JSON.parse(lsRanking); 
+  }
+  ranking.push(nuevoPuntaje);
+  ranking.sort(function(intentosA,intentosB){
+    return intentosA.intentos - intentosB.intentos;
+  })
+  ranking = ranking.slice(0,5);
+  localStorage.setItem(lsRankingKey, JSON.stringify(ranking));
+  console.log(ranking);
+  return ranking; 
+}
+
+// Funcion para dibujar tabla de ranking
+
+function dibujarRanking(ranking) {
+   var tableRow = "<tr><td></td><td></td><td></td></tr>";
+   ranking.forEach(function(puntaje) {
+     tablaRanking.append("<tr><td>" + puntaje.nombre + "</td><td>" + puntaje.nivel + "</td><td>" + puntaje.intentos + "</td></tr>");
+   });
+} 
+
+
+
 
 // Funcion para agregar clase oculto y remover clase oculto
 
-function ocultarElementos(...jqElements) {
+function hideElements(...jqElements) {
   for(var i = 0; i < jqElements.length; i++) {
     if (jqElements[i] != null || jqElements[i] != undefined) {
       jqElements[i].addClass('oculto');
@@ -195,14 +239,14 @@ function ocultarElementos(...jqElements) {
   }
 }
 
-function mostrarElementos(...jqElements) {
+function showElements(...jqElements) {
   for(var i = 0; i < jqElements.length; i++) {
     if (jqElements[i] != null || jqElements[i] != undefined) {
       jqElements[i].removeClass('oculto');
     }
   }
 }
-function mostrarElemento(...jqElements) {
+function addOpacity(...jqElements) {
   for(var i = 0; i < jqElements.length; i++) {
     if (jqElements[i] != null || jqElements[i] != undefined) {
       jqElements[i].addClass('opacity');
@@ -210,40 +254,10 @@ function mostrarElemento(...jqElements) {
   }
 }
 
-function ocultarElemento(...jqElements) {
+function removeOpacity(...jqElements) {
   for(var i = 0; i < jqElements.length; i++) {
     if (jqElements[i] != null || jqElements[i] != undefined) {
       jqElements[i].removeClass('opacity');
     }
   }
 }
-
-
-
-// defino la constante que guarda la key que quiero usar para
-// traer y guardar el ranking en LocalStorage
-// const lsRankingKey = "memotest-ranking"
-
-// creo el array vacio de info de partida donde voy a guardar y
-// actualizar la info del ranking
-// var ranking = [];
-
-// me traigo el ranking de local storage.
-// >>> ME DEVUELVE UN STRING
-// var lsRankingString = localStorage.getItem(lsRankingKey);
-
-
-// // si el string NO es null y el string NO esta vacio
-// if (lsRankingString != null && lsRankingString != "") {
-//   // lo parseo y lo convierto en un array de objetos de ranking
-//   // y lo guardo en la variable "ranking"
-//   ranking = JSON.parse(lsRankingString);
-// }
-
-
-
-
-
-
-
-
